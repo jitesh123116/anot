1cefe3c95ee35868fc2e008cd19e42ceb68f1b7a
jitesh123116 <jitesh.kumar@affle.com>
2018-04-10 14:22:39 +0530

    anot

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
-	//url: 'mongodb://127.0.0.1:27017/annotation'
-      url: 'mongodb://annot:1234@ds231229.mlab.com:31229/annotation'
+	url: 'mongodb://127.0.0.1:27017/annotation'
+	  
+	//url: 'mongodb://annot:1234@ds231229.mlab.com:31229/annotation'
 
 }
